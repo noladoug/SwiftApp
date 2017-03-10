2c463a2791d7fdcb749632b77f7c1a5323131293
Doug Frye <noladoug@aol.com>
2017-03-10 14:15:08 -0600

    Added text fields and calculation button.

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -11,27 +11,22 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
-    
-    var tapCount = 0
 
-    @IBAction func buttonTapped(_ sender: Any) {
-     theLabel.text = "Hello There Bob!"
-        tapCount = tapCount + 1
-        print("Button tapped")
-        if tapCount >= 10 {
-            theLabel.text = "You tapped the button 10 times!"
-        }
+    @IBOutlet weak var text1: UITextField!
     
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+     theLabel.text = "Hello There Doug!"
+        print(text1.text!)
+        print(text2.text!)
+        theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
     }
 
-    @IBAction func buttonTwoTapped(_ sender: Any) {
-        theLabel.text = "Buttons are cool!"
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
