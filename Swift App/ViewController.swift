68c9ea6ba23e18fbc69f7085950e1186b9fa06ed
Doug Frye <noladoug@aol.com>
2017-03-10 12:36:05 -0600

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+
+    @IBAction func buttonTapped(_ sender: Any) {
+     theLabel.text = "Hello There Bob!"
+        tapCount = tapCount + 1
+        print("Button tapped")
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button 10 times!"
+        }
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func buttonTwoTapped(_ sender: Any) {
+        theLabel.text = "Buttons are cool!"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +39,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
