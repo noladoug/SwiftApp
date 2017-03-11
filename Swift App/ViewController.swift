a05ae9b8ab73027d8b2ccd35aa90763e41d84500
Doug Frye <noladoug@aol.com>
2017-03-11 09:10:47 -0600

    Added boolean check for plus/minus

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,30 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
-
+    
     @IBOutlet weak var text1: UITextField!
     
     @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
-     theLabel.text = "Hello There Doug!"
-        print(text1.text!)
-        print(text2.text!)
-        theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        theLabel.text = "Hello There Doug!"
+        
+        let addition = false
+        
+        if addition {
+            theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            theLabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
 
